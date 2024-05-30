76d3d93c2334a77ebc5f6796462e1aade2875f50
achyut <achyutp10@gmail.com>
2024-05-30 15:15:00 +0545

    conditions_challenge

---
+++ 4_condition_challenge.py
@@ -0,0 +1,15 @@
+a = int(input("Enter first no: "))
+b = int(input("Enter first no: "))
+
+difference1 = a-b
+difference2 = b-a
+if difference1 > 0:
+  print(difference1)
+
+else:
+  print(difference2)
+# else:
+#   c=a
+#   a=b
+#   b=c
+#   print(a-b)
