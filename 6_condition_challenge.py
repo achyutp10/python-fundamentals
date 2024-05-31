3fb5e9b433cecb973831259b2ec32d4eb6739b74
achyut <achyutp10@gmail.com>
2024-05-31 11:44:00 +0545

    condition_challenge

---
+++ 6_condition_challenge.py
@@ -0,0 +1,30 @@
+# age = int(input("Enter age: "))
+
+# if age>=18:
+#   print('Can vote')
+# else: 
+#   print('Cannot vote')
+
+# marks = int(input("Enter marks: "))
+
+# if marks>=0 and marks<=100:
+#   print('Valid')
+# else:
+#   print('invalid')
+
+
+# gender = (input("Enter gender: "))
+
+# if gender=='M' or gender=='m':
+#   print('Male')
+# elif gender=='F' or gender=='f':
+#   print('female')
+# else:
+#   print('Invalid')
+
+age = int(input("Enter age: "))
+
+if age>=18 and age<=60:
+  print('Eligible')
+else:
+  print('Not eligible')
