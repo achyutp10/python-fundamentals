ca265e82571c032c4a05f491c6a8d99075f09fcd
achyut <achyutp10@gmail.com>
2024-05-31 11:44:39 +0545

    condition_challenge_next

---
+++ 7_condition_challenge.py
@@ -0,0 +1,23 @@
+
+# math = int(input("Enter marks for math: "))
+# phy = int(input("Enter marks for phy: "))
+# chem = int(input("Enter marks for chem: "))
+
+# if math>=45 and phy>=45 and chem >=45:
+#   print('Pass')
+# else:
+#   print('Not pass') 
+
+# name = (input("Enter Username: "))
+
+# if name=='Smith' or name=='smith' or name=='John' or name=='john':
+#   print('Authorized')
+# else:
+#   print('Not Authorized') 
+
+letter = (input("Enter character: "))
+
+if letter=='a' or letter=='e' or letter=='i' or letter=='o' or letter=='u':
+  print('Vowel')
+else:
+  print('Consonant') 
