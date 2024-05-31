5556c3791c4282bd4734057e246411b40a6194b8
achyut <achyutp10@gmail.com>
2024-05-31 11:46:29 +0545

    10_while

---
+++ 10_while.py
@@ -0,0 +1,16 @@
+# c = 0
+# while c<5:
+#   print(c, 'Hello')
+#   c = c+1
+
+# n = 1245
+
+# while n>0:
+#   r=n%10
+#   n=n//10
+  
+#   print(r)
+
+# a = 54
+# print(a//10) #--> 5
+# print(a/10) #--> 5.4
