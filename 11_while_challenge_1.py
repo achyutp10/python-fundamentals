4478ccceed003de9c0173ae6f551da579be2148f
achyut <achyutp10@gmail.com>
2024-06-01 10:55:53 +0545

    while challenge

---
+++ 11_while_challenge_1.py
@@ -0,0 +1,22 @@
+# c = 0
+# while c<5:
+#   print(c, 'Hello')
+#   c = c+1
+
+# n = 1245
+
+# while n>0:
+#   r=n%10
+#   n=n//10
+  
+#   print(r)
+
+# a = 54
+# print(a//10) #--> 5
+# print(a/10) #--> 5.4
+
+n = int(input("Enter a number: "))
+c = 1
+while c<=10:
+  print(n, 'x', c, '=', n*c)
+  c=c+1
