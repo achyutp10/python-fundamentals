b52d24d9062bb7b517d92434199cfc48e9669468
achyut <achyutp10@gmail.com>
2024-06-01 10:57:54 +0545

    break_continue_pass

---
+++ 15_break_continue_pass.py
@@ -0,0 +1,39 @@
+# while True:
+#   n=5
+#   if n>0:
+#     print("Pos")
+#     break
+#   elif n<0:
+#     print("Neg")
+#     break
+  
+# c = 0
+# while c<10:
+#   print(c)
+#   c=c+1
+#   if c>5:
+#     break
+
+# c = 0
+# while c<10:
+#   n=int(input("Enter: "))
+#   if n%3==0:
+#     continue
+#   print(n)
+#   c=c+1
+
+# c = 0
+# while c<10:
+#   n=int(input("Enter: "))
+#   if n%3==0:
+#     pass
+#   else:
+#     print(n)
+#   c=c+1
+
+c = 0
+while c<10:
+  n=int(input("Enter: "))
+  if n%3 !=0:
+    print(n)
+  c=c+1
