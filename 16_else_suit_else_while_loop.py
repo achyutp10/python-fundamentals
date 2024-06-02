970d6d3b94e1f8032ab8fd7c1246f529af15defe
achyut <achyutp10@gmail.com>
2024-06-02 10:03:25 +0545

    else_suit_else_while_loop

---
+++ 16_else_suit_else_while_loop.py
@@ -0,0 +1,17 @@
+# c = 1
+# while c<=10:
+#   print(c)
+#   c=c+1
+# else:
+#   print("Printed")
+# print('End')
+
+c = 1
+while c<=10:
+  print(c)
+  c=c+1
+  if c>5:
+    break
+else:
+  print("Printed")
+print('End')
