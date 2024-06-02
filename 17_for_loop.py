edf855a6954900e5aabc43419f348f9f23f94cbb
achyut <achyutp10@gmail.com>
2024-06-02 10:03:56 +0545

    for_loop

---
+++ 17_for_loop.py
@@ -0,0 +1,24 @@
+# msg = 'Hello'
+# for i in msg:
+#   print(i)
+
+# a = 'Hello'
+# for i in range(len(a)):
+#   print(i)
+
+range(10)
+range(5,10) #start end
+range(2,8,3) #start end step
+range(10,0,-1)
+
+# for i in range(5):
+#   print(i)
+
+# for i in range(5,10):
+#   print(i)
+
+# for i in range(5,10,2):
+#   print(i)
+
+for i in range(10,0,-2):
+  print(i)
