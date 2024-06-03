d43ba70d856f3aab932530f2fbfb11b1a6820c02
achyut <achyutp10@gmail.com>
2024-06-03 18:36:20 +0545

    break_continue_pass_for_loop

---
+++ 21_break_continue_pass_for_loop.py
@@ -0,0 +1,13 @@
+# for i in range(0,10):
+#   if i<5:
+#     print(i)
+#   else:
+#     break
+# else:
+#   print("Completed")
+
+for i in range(0,6):
+  if i%5 == 0:
+    continue
+  else:
+    print(i)
