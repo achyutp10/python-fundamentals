ba72e026274d5f2744b85aa92d3f174a41404f9a
achyut <achyutp10@gmail.com>
2024-06-03 18:37:48 +0545

    pattern

---
+++ 24_pattern.py
@@ -0,0 +1,19 @@
+# for i in range(0,5):
+#   for j in range(0,5):
+#     print("*", end=' ')
+#   print('')
+
+# for i in range(0,5):
+#   print('* '*5)
+
+for i in range(5,0,-1):
+  print('* '*(i))
+
+for i in range(0,5):
+  print('* '*(i+1))
+
+# for i in range(0,5):
+#   for j in range(0,5):
+#     if i>=j:  
+#       print("*", end=' ')
+#   print('')
