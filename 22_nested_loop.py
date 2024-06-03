a1d2b2dae7a07938727205881d67d6d82d603b57
achyut <achyutp10@gmail.com>
2024-06-03 18:36:52 +0545

    nested_loop

---
+++ 22_nested_loop.py
@@ -0,0 +1,13 @@
+# for i in range(0,5):
+#   for j in range(0,5):
+#     if i>=j:
+#     # print('(',i+j,')', end=',')
+#       print("*", end=' ')
+#   print('')
+
+s1 = 'abc'
+s2 = 'xyz'
+for i in s1:
+  for j in s2:
+    print(i,j, end=' ') 
+  print('')
