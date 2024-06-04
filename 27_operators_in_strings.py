73a4fa625b5a60434409bbbd3beb98d934ac0171
achyut <achyutp10@gmail.com>
2024-06-04 12:21:42 +0545

    operators_in_strings

---
+++ 27_operators_in_strings.py
@@ -0,0 +1,43 @@
+s1 = 'Hello World'
+# s2 = 'World'
+# s3 = s1+s2
+# print(s3)
+# s4 = 'Hello' 'World'
+# print(s4)
+# s5 = 'Hello' +str(15)
+# print(s5)
+# s6='Hi'
+# print(s6*4)
+
+# for x in s1:
+#   print(x)
+
+# for i in range(0, len(s1)):
+#   print(s1[i])
+
+# for i in range(0, len(s1),2):
+#   print(s1[i])
+  # print('H'in s1)
+
+# s1[start:end:stop]
+
+print(s1[4:5])
+# print(s1[0:len(s1):1])
+# print(s1[::1])
+# print(s1[::])
+# print(s1[3::])
+# print(s1[6:8])
+# print(s1[::2])
+# print(s1[::-1])
+# print(s1[-1:len(s1)-1:-1])
+# print(s1[-1::-1])
+# print(s1[-1::-2])
+
+# print('H' in s1)
+# print('Wo' in s1)
+# print('Me' in s1)
+# print('Me' not in s1)
+# print(' ' not in s1)
+# print(' ' in s1)
+# print('Hello' in s1)
+# print('Hello ' in s1)
