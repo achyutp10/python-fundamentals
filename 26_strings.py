018781621491daab573fb5aaeeaf310cf995325c
achyut <achyutp10@gmail.com>
2024-06-04 12:21:08 +0545

    strings

---
+++ 26_strings.py
@@ -0,0 +1,19 @@
+s1="hello's"
+# print(s1[0])
+# print(type(s1))
+# a = str(input("enter: "))
+# print(a)
+# print(type(a))
+# print(len(s1))
+
+for i in s1:
+  print(i)
+
+for i in range(len(s1)):
+  print(i)
+
+s2 = "Achyut's"
+s3 = '''hello
+        world'''
+
+print(len(s3))
