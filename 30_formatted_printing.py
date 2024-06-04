aac0f473fb8dd218c73331d4d08fe36d320aa670
achyut <achyutp10@gmail.com>
2024-06-04 12:23:42 +0545

    formatted_printing

---
+++ 30_formatted_printing.py
@@ -0,0 +1,25 @@
+# a=1
+# b='abc'
+# c=86.9
+# print("Hello %s, hi %d, hy %f" % (b,a,c))
+
+# x=22
+# y=7
+# z=x/y
+# print('Div of {} & {} is {}'.format(x,y,z))
+# print('Div of {0} & {1} is {2}'.format(x,y,z))
+# print('Div of {2} & {1} is {0}'.format(x,y,z))
+# print('Div of {2} & {1} is {0}'.format(z,y,x))
+# print('Div of {0:10} & {1:15} is {2:2.4}'.format(x,y,z))
+# print('Div of {0:<10} & {1:^15} is {2:2.4}'.format(x,y,z))
+
+# x=1234567890
+# print('{0:20,}'.format(x))
+# print('Div of {0:<10} & {1:^15} is {2:E}'.format(x,y,z))
+# print('Div of {0:<10} & {1:^15} is {2:F}'.format(x,y,z))
+# print('Div of {0:<10} & {1:^15} is {2:G}'.format(x,y,z))
+
+# print(f'Division of {x:10} & {y:^15} is {z:2.4f}')
+
+from re import *
+print(match('a','a').group())
