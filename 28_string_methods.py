b08c7b981527d6d9d85f3b4d44c6914269c7d921
achyut <achyutp10@gmail.com>
2024-06-04 12:22:44 +0545

    28_string_methods

---
+++ 28_string_methods.py
@@ -0,0 +1,111 @@
+s = 'Hello, how are you'
+
+# print(type(s))
+
+# print(dir(str))
+# print(dir(int))
+
+# print(help(s.capitalize))
+
+# s.find(sub[,start[,end]])
+# s.rfind(sub[,start[,end]])
+# s.index(sub[,start[,end]])
+# s.rindex(sub[,start[,end]])
+# s.count(sub[,start[,end]])
+
+# print(s.find('o'))
+# print(s.find('o',5))
+# print(s.find('o',5,7))
+# print(s.find('how'))
+# print(s.find('k'))
+
+# print(s.rfind('o'))
+# print(s.rfind('o',0,15))
+# print(s.rfind('kite'))
+
+# print(s.rindex('o'))
+# print(s.rindex('o',0,15))
+# print(s.rindex('kite'))
+
+# print(s.count('o'))
+# print(s.count('how',4,15))
+# print(s.count('k',7,15))
+
+# s="python"
+# print(s.ljust(8,"*"))
+# print(s.rjust(10,'*'))
+# print(s.center(10,'*'))
+# print(s.ljust(8,"*"))
+
+# s="****python****"
+# print(s.lstrip('*'))
+# print(s.strip('*'))
+# print(s.rstrip('*'))
+
+# s1='.... ... ++aaapython'
+# print(s1.lstrip('.'))
+# print(s1.lstrip('. +'))
+# print(s1.lstrip(' .+a'))
+
+# s = 'hello dear'
+# print(s.capitalize())
+
+# s = 'Hello'
+# print(s.lower())
+
+# s = 'HeLLo'
+# print(s.upper())
+
+# s = 'hEllO'
+# print(s.title())
+
+# s = 'HEEllO'
+# print(s.swapcase())
+
+# s = 'HELLO'
+# print(s.casefold())
+
+# Inquiry methods
+
+# print(s.isupper()) # s = 'HELLO' True
+# print(s.islower()) # s = 'hellO' False
+# print(s.istitle()) # s = 'Hello Bro' True
+# print(s.isalnum()) # s = 'hellO123' True h-1 F
+# print(s.isalpha())
+# print(s.isspace())
+# print(s.isascii())
+# print(s.isidentifier())
+# print(s.isprintable())
+# print(s.isdecimal())
+# print(s.isdigit())
+# print(s.isnumeric())
+
+# print(s.startswith('Char',start, end))
+# print(s.startswith('He',0))
+# print(s.endswith())
+
+# print(s.removesuffix('He'))
+# print(s.removesuffix())
+# print(s.removeprefix())
+
+# print(s.partition('He'))
+# print(s.partition('ar'))
+# print(s.rpartition())
+
+s='a-b-c-d-e'
+s='a b c d e'
+# s='xyz'
+y='abc'
+a = ['john', 'smith', 'ajay']
+s1='-'
+print(s.replace('-',','))
+print(s.replace('-',',',3))
+print(s.join(y))
+print(s1.join(a))
+print(s.split())
+print(s.split(','))
+print(s.split(',',1))
+print(s.rsplit())
+print(s.splitlines())
+
+
